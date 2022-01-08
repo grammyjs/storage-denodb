ff69f9d38b578477d5841f869650e9883398dd0b
Satont <satontworldwide@gmail.com>
2022-01-08 23:03:26 +0300

    chore: update dependencies

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export type { StorageAdapter } from "https://deno.land/x/grammy@v1.3.4/mod.ts"
-export { Database, DataTypes, Model } from "https://deno.land/x/denodb@v1.0.39/mod.ts";
+export type { StorageAdapter } from "https://deno.land/x/grammy@v1.6.1/mod.ts"
+export { Database, DataTypes, Model } from "https://deno.land/x/denodb@v1.0.40/mod.ts";
