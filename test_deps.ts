b196c4cb4ab944138a913e8a7a83a38249f44288
Satont <satontworldwide@gmail.com>
2022-01-08 23:24:21 +0300

    tests: use e2e tests instead of units

--- test_deps.ts
+++ test_deps.ts
@@ -1,2 +1,4 @@
-export { assertEquals } from "https://deno.land/std@0.113.0/testing/asserts.ts";
-export { SQLite3Connector } from "https://deno.land/x/denodb@v1.0.39/mod.ts";
+export { SQLite3Connector } from "https://deno.land/x/denodb@v1.0.40/mod.ts";
+export { Bot, Context, session } from "https://deno.land/x/grammy@v1.6.1/mod.ts"
+export type { SessionFlavor } from 'https://deno.land/x/grammy@v1.6.1/mod.ts'
+export { expect } from 'https://deno.land/x/expect@v0.2.9/mod.ts'
